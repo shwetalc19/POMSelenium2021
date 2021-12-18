b502e89c4915e28a2a5a9b5f7f3c6e56ff6f2733
shwetalc18 <shwetalc18@gmail.com>
2021-12-18 11:55:23 -0500

    constant files changed

--- src/main/java/com/qa/opencart/utils/Constants.java
+++ src/main/java/com/qa/opencart/utils/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
 
 	public static final String LOGIN_PAGE_TITLE = "Account Login";
 	public static final String LOGIN_PAGE_URL_FRACTION = "route=account/login";
-	public static final int DEFAULT_TIME_OUT = 7;
+	public static final int DEFAULT_TIME_OUT = 5;
 	public static final String ACCOUNT_PAGE_TITLE = "My Account";
 	public static final String ACCOUNT_PAGE_HEADER = "Your Store";
 	public static final int IMAC_IMAGE_COUNT = 3;
