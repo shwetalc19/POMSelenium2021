e52839985a6c152602a88c1ac004745683b541d1
shwetalc18 <shwetalc18@gmail.com>
2021-12-18 10:42:09 -0500

    files changed

--- src/main/java/com/qa/opencart/utils/Constants.java
+++ src/main/java/com/qa/opencart/utils/Constants.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Constants {
 
-	public static final String LOGIN_PAGE_TITLE = "Account Login**";
+	public static final String LOGIN_PAGE_TITLE = "Account Login";
 	public static final String LOGIN_PAGE_URL_FRACTION = "route=account/login";
 	public static final int DEFAULT_TIME_OUT = 5;
 	public static final String ACCOUNT_PAGE_TITLE = "My Account";
@@ -14,7 +14,7 @@ public class Constants {
 	public static final int MACBOOKPRO_IMAGE_COUNT = 40;
 	public static final int MACBOOKAIR_IMAGE_COUNT = 4;
 	public static final String LOGIN_ERROR_MSG = "No match for E-Mail Address and/or Password.";
-	public static final String REGISTRATION_SUCCESS_MSG = "Your Account Has Been Created!**";
+	public static final String REGISTRATION_SUCCESS_MSG = "Your Account Has Been Created!";
 	public static final String REGISTER_SHEET_NAME = "registration";
 	
 	public static List<String> getExpectedAccSectionList ()
